9dad08438a8e2b7147c3a1cfc23306893546c3c3
dwirput <dwirput@gmail.co>
2021-10-16 02:27:15 +0700

    react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render(){
+    const i =1
+    var myStyle = {
+      fontSize :100,
+      color : '#FF0000'
+    }
+    return(
+      <div>
+      <h1> Hearder </h1>
+      <h2> Content </h2>
+      <p data-myattribute = "somevalue"> this is the content !!</p>
+      <p> {1+1} </p>
+      <p style = {myStyle}> {i == 1 ? 'True' : 'false'} </p>
+      </div>
+    );
+  }
 }
 
 export default App;
